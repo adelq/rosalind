41331b5367eebea6e4a72f429cd3d4ae6cc00095
Adel Qalieh <aqalieh95@gmail.com>
2016-05-15 13:31:28 -0400

    Ordering Strings of Varying Length Lexicographically

--- LEXV.py
+++ LEXV.py
@@ -1,18 +1,21 @@
 from itertools import product
 
-def main(alpha, num):
-	orderedalpha = alpha.replace(" ", "")
-	
-	# Generate all the permutations
-	permtuple = []
-	for i in range(1, num+1):
-		permtuple += ([x for x in product(orderedalpha, repeat=i)])
 
-	# Output permutations properly formatted
-	permlist = []
-	for i in permtuple:
-		permlist += ''.join(map(str, i))
-	print permlist
+def main(alpha, num):
+    orderedalpha = alpha.replace(" ", "")
+
+    # Generate all the permutations
+    permtuple = []
+    for i in range(1, num + 1):
+        permtuple += list(product(orderedalpha, repeat=i))
+
+    # Sort list by lexicographic ordering
+    permlist = ["".join(word) for word in permtuple]
+    permlist.sort(key=lambda word: [orderedalpha.index(c) for c in word])
+
+    # Output formatted list
+    for word in permlist:
+        print word
 
 # Test
 main("D N A", 3)
