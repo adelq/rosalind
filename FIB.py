889f94c5d9505e52d55592de1f2868cf4cbe9946
Adel Qalieh <aqalieh95@gmail.com>
2017-12-02 14:09:01 -0500

    Add dynamic programming solution for fibonacci rabbits

--- FIB.py
+++ FIB.py
@@ -1,10 +1,24 @@
-def wabbits(n, k):
-	if n == 0:
-		return 0
-	if n == 1:
-		return 1
-	else:
-		return wabbits(n-1, k) + k*wabbits(n-2, k)
+from __future__ import print_function
 
-#Test
-print wabbits(5, 3)
+
+def wabbits(n, k):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    else:
+        return wabbits(n-1, k) + k*wabbits(n-2, k)
+
+
+def wabbits_dynamic(n, k):
+    """Dynamic programming solution"""
+    prev_vals = [0, 1]
+    for i in range(2, n + 1):
+        next_val = k * prev_vals[0] + prev_vals[1]
+        prev_vals[0] = prev_vals[1]
+        prev_vals[1] = next_val
+    return prev_vals[1]
+
+
+# Test
+print(wabbits(5, 3))
